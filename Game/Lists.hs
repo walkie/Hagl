044ff67c4f20becba49fc93c636885b73d3a014a
Eric Walkingshaw <eric@walkingshaw.net>
2009-05-10 22:03:07 -0700

    (should have added a few patches ago) Lists.hs contains all ByGame, ByTurn, ByPlayer stuff.  By* now instances of Functor.
    Ignore-this: 21e39fbc5d12f579beda8da90fda590a
    
    darcs-hash:20090511050307-76698-77217af454736668ee2a2008da573ebd13a6b1e3.gz

---
+++ Game/Lists.hs
@@ -0,0 +1,65 @@
+
+module Game.Lists where
+
+import Control.Monad
+
+data ByGame a = ByGame [a] deriving (Eq, Show)
+data ByTurn a = ByTurn [a] deriving (Eq, Show)
+data ByPlayer a = ByPlayer [a] deriving (Eq, Show)
+
+forGame :: ByGame a -> Int -> a
+forGame (ByGame as) i = as !! (length as - i)
+
+forTurn :: ByTurn a -> Int -> a
+forTurn (ByTurn as) i = as !! (length as - i)
+
+forPlayer :: ByPlayer a -> Int -> a
+forPlayer (ByPlayer as) i = as !! (i-1)
+
+forGameM :: Monad m => m (ByGame a) -> Int -> m a
+forGameM l i = liftM (flip forGame i) l
+
+forTurnM :: Monad m => m (ByTurn a) -> Int -> m a
+forTurnM l i = liftM (flip forTurn i) l
+
+forPlayerM :: Monad m => m (ByPlayer a) -> Int -> m a
+forPlayerM l i = liftM (flip forPlayer i) l
+
+class DList d => ByGameOrTurn d where
+  forGameOrTurn  :: d a -> Int -> a
+  forGameOrTurnM :: Monad m => m (d a) -> Int -> m a
+instance ByGameOrTurn ByGame where
+  forGameOrTurn = forGame
+  forGameOrTurnM = forGameM
+instance ByGameOrTurn ByTurn where
+  forGameOrTurn = forTurn
+  forGameOrTurnM = forTurnM
+
+game's = undefined :: ByGame a
+games' = undefined :: ByGame a
+
+turn   = undefined :: ByTurn a
+turn's = undefined :: ByTurn a
+turns' = undefined :: ByTurn a
+
+instance Functor ByGame where
+  fmap f (ByGame as) = ByGame (map f as)
+instance Functor ByTurn where
+  fmap f (ByTurn as) = ByTurn (map f as)
+instance Functor ByPlayer where
+  fmap f (ByPlayer as) = ByPlayer (map f as)
+
+class Functor d => DList d where
+  toList :: d a -> [a]
+instance DList ByGame where
+  toList (ByGame as) = as
+instance DList ByTurn where
+  toList (ByTurn as) = as
+instance DList ByPlayer where
+  toList (ByPlayer as) = as
+
+toList2 :: (DList f, DList g) => f (g a) -> [[a]]
+toList2 = map toList . toList
+
+toList3 :: (DList f, DList g, DList h) => f (g (h a)) -> [[[a]]]
+toList3 = map toList2 . toList
