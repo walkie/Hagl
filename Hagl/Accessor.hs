46dfb51b02bccda34494a81204607bf360a9cf16
Eric Walkingshaw <eric@walkingshaw.net>
2009-06-16 19:57:32 -0700

    Working on Accessors.  Crazy type family errors...
    Ignore-this: e0411048408c722f7d7f07f732fe560e
    
    darcs-hash:20090617025732-76698-b84fd6a14ee7e0dd09bed62e085fb6b403a2063d.gz

--- Hagl/Accessor.hs
+++ Hagl/Accessor.hs
@@ -1,65 +1,103 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
-module Game.Strategy.Accessor where
+module Hagl.Accessor where
 
 import Control.Monad
 import Data.List
-import Game.Definition
-import Game.Execution
-import Game.Execution.Util
-import Game.Lists
+import Data.Maybe    (fromMaybe)
+
+import Hagl.Core
 
 --------------------
 -- Data Accessors --
 --------------------
 
-game :: GameMonad m mv => m (Game mv)
-game = liftM _game getExecState
+--
+-- Core accessors
+--
 
-players :: GameMonad m mv => m [Player mv]
-players = liftM _players getExecState
+game :: (Game g, GameM m g) => m g
+game = liftM _game getExec
 
-numMoves :: GameMonad m mv => m (ByPlayer Int)
-numMoves = liftM _numMoves getExecState
+players :: (Game g, GameM m g) => m [Player g]
+players = liftM _players getExec
 
-location :: GameMonad m mv => m (InfoGroup mv)
-location = getExecState >>= \s -> return $ info (_game s) (_location s)
+gameState :: (Game g, GameM m g) => m (State g)
+gameState = liftM _gameState getExec
 
-transcript :: GameMonad m mv => m (Transcript mv)
-transcript = liftM _transcript getExecState
+playerIx :: (Game g, GameM m g) => m (Maybe PlayerIx)
+playerIx = liftM _playerIx getExec
 
-history :: GameMonad m mv => m (History mv)
-history = liftM _history getExecState
+transcript :: (Game g, GameM m g) => m (Transcript g)
+transcript = liftM _transcript getExec
 
-finished :: GameMonad m mv => m Int
-finished = liftM (length . toList) history
+history :: (Game g, GameM m g) => m (History g)
+history = liftM _history getExec
 
-gameNumber :: GameMonad m mv => m Int
-gameNumber = liftM (+1) finished
+numMoves :: (Game g, GameM m g) => m (ByPlayer Int)
+numMoves = liftM _numMoves getExec
+
+--
+-- Other accesssors
+--
+
+-- The index of the currently active player.
+myIx :: (Game g, GameM m g) => m PlayerIx
+myIx = liftM (fromMaybe e) playerIx
+  where e = error "Internal error: playerIx not set!"
+
+-- The number of players playing the game.
+numPlayers :: (Game g, GameM m g) => m Int
+numPlayers = liftM length players
+
+-- The number of completed games.
+numGames :: (Game g, GameM m g) => m Int
+numGames = liftM (length . toList) history
+
+-- The current game number.
+gameNumber :: (Game g, GameM m g) => m Int
+gameNumber = liftM (+1) numGames
 
 -- True if this is the first iteration in this execution instance.
-isFirstGame :: GameMonad m mv => m Bool
-isFirstGame = liftM (null . toList) history
+isFirstGame :: (Game g, GameM m g) => m Bool
+isFirstGame = liftM (> 1) gameNumber
 
-transcripts :: GameMonad m mv => m (ByGame (Transcript mv))
+-- Transcript of each game.
+transcripts :: (Game g, GameM m g) => m (ByGame (Transcript g))
 transcripts = do ts <- liftM _transcripts history
                  t  <- transcript
                  return (t `mcons` ts)
+{-
+transcripts :: (Game g, GameM m g) => m (ByGame (Transcript g))
+transcripts = do h  <- history
+                 t' <- transcript
+                 return (ByGame (t' : [t | (t,_) <- toList h]))
+-}
 
-summary :: GameMonad m mv => m (Summary mv)
-summary = liftM2 summarize game transcript
+-- MoveSummary of the current (incomplete) game.
+moveSummary :: (Game g, GameM m g) => m (MoveSummary g)
+moveSummary = do np <- numPlayers
+                 t  <- transcript
+                 (return . ByPlayer . map (forp t)) [1..np]
+  where forp t i = ByTurn [mv | (mi,mv) <- t, mi == Just i]
 
-summaries :: GameMonad m mv => m (ByGame (Summary mv))
+-- Summary of the current (incomplete) game.
+summary :: (Game g, GameM m g) => m (Summary g)
+summary = do ms <- moveSummary 
+             return (ms, Nothing)
+
+-- Summary of each game.
+summaries :: (Game g, GameM m g) => m (ByGame (Summary g))
 summaries = do ss <- liftM _summaries history
                s  <- summary
                return (s `mcons` ss)
 
 -- All moves made by each player in each game.
-moves :: GameMonad m mv => m (ByGame (ByPlayer (ByTurn mv)))
+moves :: (Game g, GameM m g) => m (ByGame (ByPlayer (ByTurn (Move g))))
 moves = liftM (fmap _moves) summaries
 
 class (ByX d, ByX e) => MoveList d e where
-  move :: GameMonad m mv => m (d (e mv))
+  move :: (Game g, GameM m g) => m (d (e (Move g)))
 instance MoveList ByGame ByPlayer where
   move = do ByGame ms <- moves
             return $ ByGame [fmap (head . toList) m | m <- tail ms]
@@ -67,10 +105,10 @@ instance MoveList ByPlayer ByTurn where
   move = liftM _moves summary
             
 -- The total payoff for each player for each game.
-payoff :: GameMonad m mv => m (ByGame (ByPlayer Float))
+payoff :: (Game g, GameM m g) => m (ByGame Payoff)
 payoff = liftM (fmap _payoff) summaries
 
 -- The current score of each player.
-score :: GameMonad m mv => m (ByPlayer Float)
+score :: (Game g, GameM m g) => m (ByPlayer Float)
 score = liftM (ByPlayer . map sum . transpose . toList2) payoff
 
