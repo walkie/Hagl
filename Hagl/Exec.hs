c2d3ff96d23d9148288a8f15fa11607a5d787858
Eric Walkingshaw <eric@walkingshaw.net>
2009-06-23 20:17:52 -0700

    Merged Exec.hs
    Ignore-this: 9a3fe029de077a50328b8751574117e9
    
    darcs-hash:20090624031752-76698-be35faab59ae9f7bf41b1ba218d52ce2bf4b0474.gz

--- Hagl/Exec.hs
+++ Hagl/Exec.hs
@@ -4,50 +4,72 @@ module Hagl.Exec where
 
 import Control.Monad.State
 import Data.List
+import Data.Function
 
 import Hagl.Core
 import Hagl.Accessor
 import Hagl.Print
 
+--------------------
+-- Game Execution --
+--------------------
+
+once :: Game g => ExecM g ()
+once = runGame >>= conclude
+
+times :: Game g => Int -> ExecM g ()
+times 0 = return ()
+times n = once >> times (n-1)
+
+conclude :: Game g => Payoff -> ExecM g ()
+conclude p = do
+    e <- getExec
+    g <- game
+    t <- transcript
+    h <- history
+    ms <- moveSummary
+    put e { _gameState = initState g,
+            _transcript = [],
+            _history = (t, (ms, Just p)) `dcons` h }
+
+--------------------------
+-- Tournament Execution --
+--------------------------
+
 -- Run a game with each successive collection of players. Aggregate the scores
 -- of all Players (based on name) and print the final scores.
-runGames :: (Show m) => Game m -> [[Player m]] -> GameExec m a -> IO ()
+-- TODO: Cleanup this function...
+runGames :: (Game g, Show (Move g)) => g -> [[Player g]] -> ExecM g a -> IO ()
 runGames g pss f = 
     let unique = nub $ concat pss
-        run ps = evalGame g ps (f >> our score)
-    in sequence (map run pss) >>= \vss ->
+        run ps = evalGame g ps (f >> liftM toList score)
+    in mapM run pss >>= \vss ->
          let pis = map (flip elemIndices (concat pss)) unique
              vs =  map (sum . map ((concat vss) !!)) pis
              (vs', ps') = unzip $ reverse $ sortTogether vs unique
          in do putStrLn "Final Scores:"
-               putStr $ scoreString ps' vs'
+               putStr $ scoreString (fromList ps') (fromList vs')
 
 -- Run a tournament where all combinations of players are played
 -- where player 1 comes from list 1, player 2 from list 2, etc.
-tournament :: (Show m) => Game m -> [[Player m]] -> GameExec m a -> IO ()
-tournament g pss = runGames g (allCombs pss)
+tournament :: (Game g, Show (Move g)) => g -> [[Player g]] -> ExecM g a -> IO ()
+tournament g pss = runGames g (cross pss)
 
 -- Run a tournament where all orders of all players are played 
 -- (including against selves).
-fullRoundRobin :: (Show m) => Game m -> [Player m] -> GameExec m a -> IO ()
-fullRoundRobin g ps = tournament g (replicate (numPlayers g) ps)
+-- TODO constant 2 should be replaced with some way to get number of players...
+fullRoundRobin :: (Game g, Show (Move g)) => g -> [Player g] -> ExecM g a -> IO ()
+fullRoundRobin g ps = tournament g (replicate 2 ps)
 
 -- Run a tournament where all unique combinations of players are played 
 -- (including against selves).
-roundRobin :: (Show m) => Game m -> [Player m] -> GameExec m a -> IO ()
-roundRobin g ps = runGames g (allUnique (replicate (numPlayers g) ps))
+-- TODO constant 2 should be replaced with some way to get number of players...
+roundRobin :: (Game g, Show (Move g)) => g -> [Player g] -> ExecM g a -> IO ()
+roundRobin g ps = runGames g (ucross (replicate 2 ps))
 
 -----------------------
 -- Utility Functions --
 -----------------------
 
-allCombs :: [[a]] -> [[a]]
-allCombs (xs:xss) = [(y:ys) | y <- xs, ys <- allCombs xss]
-allCombs [] = [[]]
-
-allUnique :: (Ord a) => [[a]] -> [[a]]
-allUnique = nub . map sort . allCombs
-
 sortTogether :: (Ord a) => [a] -> [b] -> [(a, b)]
-sortTogether as bs = let f (a1,_) (a2,_) = compare a1 a2
-                     in sortBy f $ zip as bs
+sortTogether as bs = sortBy (compare `on` fst) (zip as bs)
