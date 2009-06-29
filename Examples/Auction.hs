62e622f92724f2d78495ee525769ad8523508dc8
Eric Walkingshaw <eric@walkingshaw.net>
2009-06-28 21:02:37 -0700

    Merged dollar auction example.
    Ignore-this: 98ed4b787f54cd13e5d5af87df53aded
    
    darcs-hash:20090629040237-76698-5d11090803745253fdd6f0eafda74e6fcdf8bf7f.gz

---
+++ Examples/Auction.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE TypeFamilies #-}
+
+{-
+
+An implementation of the Dollar Auction:
+  http://en.wikipedia.org/wiki/Dollar_auction
+
+Example experiments from GHCi:
+> execGame Auction [p1, p2, p3, dim] (once >> printTranscript)
+> execGame Auction [p1, p2, p3, dim] (times 100 >> printScore)
+
+(The included strategies are not necessarily good ones and are
+merely included as examples...)
+
+-}
+module Examples.Auction where
+
+import Hagl
+
+type Cents = Int
+
+data Bid = Bid Cents | Pass deriving (Eq, Show)
+
+data Auction = Auction
+
+data HighBids = HighBids {
+  highBidder :: PlayerIx, highBid :: Cents,
+  nextBidder :: PlayerIx, nextBid :: Cents
+}
+
+instance Game Auction where
+  type Move Auction = Bid
+  type State Auction = HighBids
+  initState _ = HighBids 0 0 0 0
+  runGame = numPlayers >>= bid 1
+
+bid p n = do
+    h <- gameState
+    if highBidder h == p then return (pay n h) else do
+    b <- decide p
+    update h p b
+    bid (nextPlayer n p) n
+  where update h p (Bid d) 
+          | d < highBid h = error "Bid is too low!"
+          | otherwise = putGameState (HighBids p d (highBidder h) (highBid h))
+        update h p Pass = return ()
+
+pay :: Int -> HighBids -> Payoff
+pay n h = ByPlayer [fromIntegral (val p) | p <- [1..n]]
+  where val p | p == highBidder h = 100 - highBid h
+              | p == nextBidder h =   0 - nextBid h
+              | otherwise         =   0
+
+-- Players
+
+p1 :: Player Auction
+p1 = "Penny" ::: do 
+    h <- gameState
+    mixed [(4, Bid (highBid h + 1)), (1, Pass)]
+
+p2 :: Player Auction
+p2 = "Nickel" ::: do
+    h <- gameState
+    return $ if highBid h < 50 then Bid (highBid h + 5) else Pass
+
+p3 :: Player Auction
+p3 = "Quarter" ::: do
+    h <- gameState
+    mixed [(1, Bid (highBid h + 25)), (3, Pass)]
+
+dim :: Player Auction
+dim = "Diminishing returns" ::: do
+    s <- gameState
+    let high = highBid s
+    mixed [(high, Pass), (100-high, Bid (high+1))]
