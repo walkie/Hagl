833757e5e5c36a912e22c4e14bbdd11c4858e506
Eric Walkingshaw <eric@walkingshaw.net>
2009-06-28 22:20:27 -0700

    Merged TicTacToe example to new deal.
    Ignore-this: c09905b5c9a42a45746666724cd03802
    
    darcs-hash:20090629052027-76698-ef87712154eeb0c26e3d4bbfaac0fb436b69eeef.gz

--- Examples/TicTacToe.hs
+++ Examples/TicTacToe.hs
@@ -1,36 +1,81 @@
--- TODO needs to be adapted to the new deal
+{-# LANGUAGE TypeFamilies #-}
+module Examples.TicTacToe where
+
+import Control.Monad (liftM)
+import Data.List     (elemIndices, transpose)
+
+import Hagl
+import Hagl.Searchable
 
 -----------------
 -- Tic Tac Toe --
 -----------------
 
-data Square = X | O | Empty deriving (Eq, Show)
+data Square = X | O | E deriving (Eq, Show)
 type Board = [Square]
-type Move = Int
+type Mark  = Int
 
-mark 1 = X
-mark 2 = O
+data TicTacToe = TicTacToe
 
-empty :: Board -> [Int]
-empty = elemIndices Empty
+-- The initial board.
+start :: Board
+start = replicate 9 E
 
-end :: Board -> PlayerIx -> Bool
-end b p = win b p || null (empty b)
+-- The mark corresponding to each player.
+xo 1 = X
+xo 2 = O
 
-avail :: Board -> PlayerIx -> [Move]
-avail b _ = empty b
+-- Get a list of empty squares.
+markable :: Board -> [Mark]
+markable = elemIndices E
 
-exec :: Board -> PlayerIx -> Move -> Board
-exec b p m = take m b ++ mark p : drop (m+1) b
+-- The player whose turn it is.
+who :: Board -> PlayerIx
+who b = if (odd . length . markable) b then 1 else 2
 
-pay :: Board -> PlayerIx -> [Float]
-pay b p | win b p = winner 2 p
-        | otherwise = tie 2
+-- Player p marks square m.
+mark :: PlayerIx -> Mark -> Board -> Board
+mark p m b = take m b ++ xo p : drop (m+1) b
 
-win :: Board -> PlayerIx -> Bool
-win b p = let h = chunk 3 b
-              v = transpose h
-              d = map (map (b !!)) [[0,4,8],[2,4,6]]
-          in or $ map (and . map (mark p ==)) (h ++ v ++ d)
+-- True if player p has won.
+win :: PlayerIx -> Board -> Bool
+win p b = any (all (xo p ==)) (h ++ v ++ d)
+  where h = chunk 3 b
+        v = transpose h
+        d = map (map (b !!)) [[0,4,8],[2,4,6]]
 
-ticTacToe = takeTurns 2 end avail exec pay (replicate 9 Empty)
+-- True if the game is over.
+end :: Board -> Bool
+end b = null (markable b) || win 1 b || win 2 b
+
+-- The payoff awarded for a final state.
+pay :: Board -> Payoff
+pay b | win 1 b   = winner 2 1
+      | win 2 b   = winner 2 2
+      | otherwise = tie 2
+
+--
+-- Game instance.
+--
+
+instance Game TicTacToe where
+  type Move TicTacToe = Mark
+  type State TicTacToe = Board
+  initState _ = start
+  runGame     = runTicTacToe
+  
+runTicTacToe :: ExecM TicTacToe Payoff
+runTicTacToe = takeTurns turn (liftM end gameState)
+  where turn p = do m <- decide p
+                    b <- updateGameState (mark p m)
+                    return (pay b)
+
+-- 
+-- Searchable instance.
+-- TODO: Add library functions to make deriving trees easy, 
+--       given helper funcions similar to above.
+-- 
+
+instance Searchable TicTacToe where
+  gameTree  _ b   = undefined
+  nextState _ b m = undefined
