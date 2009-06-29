93a7ef7641ff7f7272f575e2811a4f5707313de9
Eric Walkingshaw <eric@walkingshaw.net>
2009-06-28 19:49:28 -0700

    Merged Hagl.hs
    Ignore-this: 1ac54a4de737ece66c5929fc9409eb9b
    
    darcs-hash:20090629024928-76698-dfb73ea33f37495c8734cd54a130f9cafbcc3f26.gz

--- Hagl.hs
+++ Hagl.hs
@@ -1,52 +1,23 @@
+-- This module provides a convenient way to import most of Hagl all at once.
+-- Modules relating to particular types of games should be imported separately,
+-- since they contain overlapping function names.  Game-specific modules include
+-- Hagl.Extensive, Hagl.Normal, and Hagl.StateBased.
 module Hagl (
-  -- Game.Definition
-  Game(..), GameTree(..), InfoGroup(..), PlayerIx,
-  normal, matrix, zerosum,
-  extensive, stateGame, takeTurns,
-  winner, loser, tie, player, (<+>), (<|>),
-  maxPlayer, availMoves, asTree,
-  children, bfs, dfs,
-  -- Game.Execution
-  History, Transcript, Summary, Event,
-  Name, Player(..), name,
-  GameExec, StratExec, Strategy, GameMonad, update,
-  -- Game.Execution.Run
-  evalGame, runGame, step, once, times,
-  -- Game.Execution.Print
-  print, printLn, printStr, printStrLn,
-  printTranscript, printTranscriptOfGame,
-  printSummaries, printSummaryOfGame, printScore,
-  -- Game.Execution.Tournament
-  runGames, tournament, fullRoundRobin, roundRobin,
-  -- Game.Lists
-  ByGame(..), ByTurn(..), ByPlayer(..), 
-  forGame, forTurn, forPlayer, forGameM, forTurnM, forPlayerM,
-  forGameOrTurn, forGameOrTurnM,
-  game's, games', turn, turn's, turns',
-  toList, toList2, toList3,
-  -- Game.Strategy
-  play, pure, randomly, randomlyFrom, mixed, periodic, minimax,
-  atFirstThen, thereafter,
-  -- Game.Strategy.Accessor
-  game, players, location, transcript, history, finished, gameNumber,
-  isFirstGame, transcripts, summaries, moves, move, payoff, score,
-  -- Game.Strategy.Selector
-  eachAnd, inThe, 
-  myIx, my, his, her, our, their,
-  this, every, first, firstn, last, lastn,
-  -- Game.Util
-  chunk
+  module Hagl.Accessor,
+  module Hagl.Core,
+  module Hagl.Exec,
+  module Hagl.Game,
+  module Hagl.GameTree,
+  module Hagl.Print,
+  module Hagl.Selector,
+  module Hagl.Strategy
 ) where
 
-import Game.Definition
-import Game.Execution
-import Game.Execution.Run
-import Game.Execution.Print
-import Game.Execution.Tournament
-import Game.Lists
-import Game.Strategy
-import Game.Strategy.Accessor
-import Game.Strategy.Selector
-import Game.Util
-
-import Prelude hiding (last, print)
+import Hagl.Accessor
+import Hagl.Core
+import Hagl.Exec
+import Hagl.Game
+import Hagl.GameTree
+import Hagl.Print
+import Hagl.Selector
+import Hagl.Strategy
