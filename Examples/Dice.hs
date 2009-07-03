03e55a5c583b8f9ea0a4ac8ccf97d07f59e45b0b
Eric Walkingshaw <eric@walkingshaw.net>
2009-07-03 14:03:03 -0700

    Added coin flipping example.
    Ignore-this: 5206597e3689e3d5ecd8166173da374a
    
    darcs-hash:20090703210303-76698-f6ee2b3e32faef6ab5593b83f5f2c192c78d83bf.gz

--- Examples/Dice.hs
+++ Examples/Dice.hs
@@ -12,3 +12,16 @@ die = Extensive 1 Perfect (Chance [(1, side a) | a <- [1..6]])
 
 -- roll n dice and print out the total
 roll n = execGame die ["Total" ::: undefined] (times n >> printScore)
+
+-------------------
+-- Coin Flipping --
+-------------------
+
+data Coin = H | T deriving (Eq, Show)
+
+coin :: GameTree Coin -> GameTree Coin -> GameTree Coin
+coin h t = Chance [(1,(H,h)), (1,(T,t))]
+
+pick :: PlayerIx -> Coin -> GameTree Coin
+pick i c = Decision i [(H, pay H), (T, pay T)]
+  where pay p = Payoff (ByPlayer [if p == c then 1 else -1])
