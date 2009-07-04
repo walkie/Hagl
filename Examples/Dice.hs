72d137007b5e1450fd061c42513d55ff31f7217c
Eric Walkingshaw <eric@walkingshaw.net>
2009-07-04 03:48:14 -0700

    Added example coin game.
    Ignore-this: 1942a5b47b12cb692562f88369800344
    
    darcs-hash:20090704104814-76698-c61f42e76688f9d7aa779520cadb11fce102dec4.gz

--- Examples/Dice.hs
+++ Examples/Dice.hs
@@ -25,3 +25,5 @@ coin h t = Chance [(1,(H,h)), (1,(T,t))]
 pick :: PlayerIx -> Coin -> GameTree Coin
 pick i c = Decision i [(H, pay H), (T, pay T)]
   where pay p = Payoff (ByPlayer [if p == c then 1 else -1])
+
+pickRight = coin (pick 1 H) (pick 1 T)
