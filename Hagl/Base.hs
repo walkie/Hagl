5d0d1ce0989983aff5366c00fdb09db96abd60c1
Eric Walkingshaw <eric@walkingshaw.net>
2010-03-24 17:03:37 -0700

    Adding Hagl.Base.Pretty.
    Ignore-this: 4b32bae9df462b6594a9f6d12bbcc6e7
    
    darcs-hash:20100325000337-76698-f4b4c1d3d0b6e6ea38e02cc508ccf8e8d8752f8c.gz

--- Hagl/Base.hs
+++ Hagl/Base.hs
@@ -1,5 +1,6 @@
 module Hagl.Base
   ( module Hagl.Base.List,
+    module Hagl.Base.Pretty,
     module Hagl.Base.Types,
     module Hagl.Base.Util,
     --module Hagl.Base.Accessor,
@@ -7,6 +8,7 @@ module Hagl.Base
   ) where
 
 import Hagl.Base.List
+import Hagl.Base.Pretty
 import Hagl.Base.Types
 import Hagl.Base.Util
 --import Hagl.Base.Accessor
