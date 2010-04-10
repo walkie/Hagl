7943a5266f4afbbf1aadc01ee8395fbee06793ca
Eric Walkingshaw <eric@walkingshaw.net>
2010-04-10 16:50:00 -0700

    Refactored tournament execution.
    Ignore-this: 28af721e50b4ad1714a6d09ba4a35968
    
    darcs-hash:20100410235000-76698-32fe7f0bbb1d4362b07e70118730131a02682f2c.gz

--- Hagl/Base/Execution.hs
+++ Hagl/Base/Execution.hs
@@ -2,9 +2,6 @@
 module Hagl.Base.Execution (step,finish,runGame) where
 
 import Control.Monad.State hiding (State)
---import Data.List
---import Data.Function
-
 import Hagl.Base.Accessor
 import Hagl.Base.Game
 import Hagl.Base.List
@@ -65,48 +62,3 @@ setPlayer i p = do e <- getExec
 setLocation :: Game g => GameTree (State g) (Move g) -> ExecM g ()
 setLocation l = do e <- getExec
                    put e { _location = l }
-
-{-
-
---------------------------
--- Tournament Execution --
---------------------------
-
--- Run a game with each successive collection of players. Aggregate the scores
--- of all Players (based on name) and print the final scores.
--- TODO: Cleanup this function...
-runGames :: (Game g, Show (Move g)) => g -> [[Player g]] -> ExecM g a -> IO ()
-runGames g pss f = 
-    let unique = nub $ concat pss
-        run ps = evalGame g ps (f >> liftM toList score)
-    in mapM run pss >>= \vss ->
-         let pis = map (flip elemIndices (concat pss)) unique
-             vs =  map (sum . map ((concat vss) !!)) pis
-             (vs', ps') = unzip $ reverse $ sortTogether vs unique
-         in do putStrLn "Final Scores:"
-               putStr $ scoreString (fromList ps') (fromList vs')
-
--- Run a tournament where all combinations of players are played
--- where player 1 comes from list 1, player 2 from list 2, etc.
-tournament :: (Game g, Show (Move g)) => g -> [[Player g]] -> ExecM g a -> IO ()
-tournament g = runGames g . cross
-
--- Run a tournament where all orders of all players are played 
--- (including against selves).
--- TODO constant 2 should be replaced with some way to get number of players...
-fullRoundRobin :: (Game g, Show (Move g)) => g -> [Player g] -> ExecM g a -> IO ()
-fullRoundRobin g ps = tournament g (replicate 2 ps)
-
--- Run a tournament where all unique combinations of players are played 
--- (including against selves).
--- TODO constant 2 should be replaced with some way to get number of players...
-roundRobin :: (Game g, Show (Move g)) => g -> [Player g] -> ExecM g a -> IO ()
-roundRobin g ps = runGames g (ucross (replicate 2 ps))
-
------------------------
--- Utility Functions --
------------------------
-
-sortTogether :: (Ord a) => [a] -> [b] -> [(a, b)]
-sortTogether as bs = sortBy (compare `on` fst) (zip as bs)
--}
---
+++ Hagl/Tournament.hs
@@ -0,0 +1,45 @@
+
+module Hagl.Tournament where
+
+import Control.Monad (liftM)
+import Data.List     (nub)
+import Data.Function (on)
+
+import Hagl.Base
+
+--------------------------
+-- Tournament Execution --
+--------------------------
+
+-- Tournaments results: mapping from player names to scores.
+type Results = [(Name,Float)]
+
+-- | Run a game with each successive collection of players. Aggregate the scores
+--   of all Players (based on name) and return the results.
+runGames :: Game g => g -> [[Player g]] -> ExecM g Payoff -> IO Results
+runGames g pss run = do
+    pays <- mapM (\ps -> evalGame g ps run) pss
+    let paymap  = zip names $ (concat . map toList) pays
+    let score n = sum [p | (m,p) <- paymap, n == m]
+    return [(n, score n) | n <- nub names]
+  where names = (map name . concat) pss
+
+-- | Tournament where all combinations of players play
+--   such that player 1 comes from list 1, player 2 from list 2, etc.
+partite :: Game g => g -> [[Player g]] -> ExecM g Payoff -> IO Results
+partite g = runGames g . cross
+
+-- | Tournament where all orders of all players are played 
+--   (including against selves).
+fullRoundRobin :: Game g => g -> Int -> [Player g] -> ExecM g Payoff -> IO Results
+fullRoundRobin g np ps = partite g (replicate np ps)
+
+-- | Tournament where all unique combinations of players are played 
+--   (including against selves). TODO test with np /= 2
+roundRobin :: Game g => g -> Int -> [Player g] -> ExecM g Payoff -> IO Results
+roundRobin g np ps = runGames g (ucross (replicate np ps))
+
+-- | Print the results of a tournament.
+printResults :: Results -> IO ()
+printResults r = do putStrLn "Tournament Results:"
+                    putStr $ unlines ["  " ++ n ++ showFloat s | (n,s) <- r]
