28d285ee0a82dd96d2ba9054c5813ee8975dae47
Eric Walkingshaw <eric@walkingshaw.net>
2010-04-10 16:14:32 -0700

    Updated times to return sum of payoffs.
    Ignore-this: f18733e202800ae75a145f1b1f6242d5
    
    darcs-hash:20100410231432-76698-e6e1de4dec2cbefd8115602c01803d17d43dc5e5.gz

--- Hagl/Iterated/Execution.hs
+++ Hagl/Iterated/Execution.hs
@@ -10,11 +10,12 @@ import Hagl.Iterated.Accessor
 -- Iterated Game Execution --
 -----------------------------
 
--- | Execute a single game iteration.
+-- | Execute a single game iteration, returning the payoff.
 once :: (Game g, Eq (Move g)) => ExecM (Iterated g) Payoff
 once = step >> gamePayoff >>= maybe once return
 
--- | Execute n game iterations.
-times :: (Game g, Eq (Move g)) => Int -> ExecM (Iterated g) ()
-times 0 = return ()
-times n = once >> times (n-1)
+-- | Execute n game iterations, returning the cummulative score.
+times :: (Game g, Eq (Move g)) => Int -> ExecM (Iterated g) Payoff
+times n = numPlayers >>= go n . tie
+  where go n p | n <= 0    = return p
+               | otherwise = once >>= go (n-1) . addPayoffs p
