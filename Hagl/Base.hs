4fa88c8e3507bf767a623c7159e285f2f714e522
Eric Walkingshaw <eric@walkingshaw.net>
2010-04-15 10:42:35 -0700

    Minor reformatting.
    Ignore-this: dd5c35d160a7efcf0e273aecf37a080e
    
    darcs-hash:20100415174235-76698-2b3f2f74681693b8133cfa61a1ca8ccdb527ff80.gz

--- Hagl/Base.hs
+++ Hagl/Base.hs
@@ -1,13 +1,13 @@
 module Hagl.Base
-  ( module Hagl.Base.Accessor,
-    module Hagl.Base.Execution,
-    module Hagl.Base.Game,
-    module Hagl.Base.List,
-    module Hagl.Base.Monad,
-    module Hagl.Base.Pretty,
-    module Hagl.Base.Selector,
-    module Hagl.Base.Strategy,
-    module Hagl.Base.Util,
+  ( module Hagl.Base.Accessor
+  , module Hagl.Base.Execution
+  , module Hagl.Base.Game
+  , module Hagl.Base.List
+  , module Hagl.Base.Monad
+  , module Hagl.Base.Pretty
+  , module Hagl.Base.Selector
+  , module Hagl.Base.Strategy
+  , module Hagl.Base.Util
   ) where
 
 import Hagl.Base.Accessor
