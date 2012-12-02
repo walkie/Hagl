34ee0a1fbe19d45211f449f66baf8b0b33605acf
Eric Walkingshaw <eric@walkingshaw.net>
2012-12-02 01:17:32 -0800

    Changing to transition graph representation of games.

--- Hagl/Base/Game.hs
+++ Hagl/Base/Game.hs
@@ -1,16 +1,33 @@
 {-# LANGUAGE TypeFamilies #-}
 
+-- | This module describes an abstract representation of games in terms
+--   of transitions between nodes.  Each node consists of a game state
+--   and an action to perform (e.g. a player must make a decision).
+--   Transitions are determined by moves and games end when a payoff
+--   node is reached.
 module Hagl.Base.Game where
 
 import Hagl.Base.List
-
+import Hagl.Base.Payoff
 
 --
--- * Game Classes
+-- * Abstract Game Representation
 --
 
--- | The most general class of games.  Instances must provide a translation
---   into the generic game tree representation.
+-- | A node consists of a game state and an action to perform.
+type Node s mv = (s, Action mv)
+
+-- | The action to perform at a given node.
+data Action mv =
+    Decision PlayerID -- ^ A decision by the indicated player.
+  | Chance (Dist mv)  -- ^ A move based on a probability distribution.
+  | Payoff Payoff     -- ^ A terminating payoff node.
+  deriving Eq
+
+
+-- | The most general class of games. Movement between nodes is captured
+--   by a transition function. This supports discrete and continuous, 
+--   finite and infinite games.
 class Game g where
   
   -- | The type of state maintained throughout the game (use @()@ for stateless games).
@@ -19,140 +36,20 @@ class Game g where
   -- | The type of moves that may be played during the game.
   type Move g
   
-  -- | The type of payoffs awarded at the end of the game.
-  type Payoff g
-
+  -- | The initial node.
   start :: g -> Node (State g) (Move g)
   
-  -- | Get the game tree representing this game.
-  gameTree :: g -> Int -> GameTree (State g) (Move g)
+  -- | The transition function.
+  transition :: g -> Node (State g) (Move g) -> Move g -> Node (State g) (Move g)
 
 
--- | Finite games have a finite number of moves available at each point
---   in the game tree.  Note that finite games may still be infinitely long.
-class Game g => FiniteGame g where
+--
+-- * Discrete Games
+--
+
+-- | Discrete games have a discrete set of moves available at each node.
+--   Note that discrete games may still be inifinite.
+class Game g => DiscreteGame g where
   
-  -- | The moves that are available from the given position in the tree.
-  movesFrom :: g -> GameTree (State g) (Move g) -> [Move g]
-
-
---
--- * Game Trees
---
-
--- | Payoffs are awarded at the end of a game. A point value for each player.
-type Payoff = ByPlayer Float
-
--- | Each position in the game tree is represented by a `Node` and has an 
---   associated state.
-data GameTree s mv = GameTree s (Node s mv) deriving Eq
-
--- | A game tree node may be either an internal node representing a decision
---   by a player or by fate, or it can be a payoff node, which ends the game.
-data Node s mv =
-    Internal (Decision mv) (mv -> GameTree s mv) -- ^ Internal node.
-  | Payoff Payoff                                -- ^ Terminating payoff.
-  deriving Eq
-
--- | A decision node represents an internal node in a game tree and
---   is either a decision by a player or a move of chance based on
---   a probability distribution.
-data Decision mv =
-    Decision PlayerID -- ^ Decision made by a player.
-  | Chance (Dist mv)  -- ^ Decision based on a distribution.
-  deriving Eq
-
-instance Game (GameTree s mv) where
-  type Move (GameTree s mv) = mv
-  type State (GameTree s mv) = s
-  gameTree g _ = g
-
-
---
--- ** Game Tree Navigation
---
-
-treeNode :: GameTree s mv -> Node s mv
-treeNode (GameTree _ n) = n
-
-treeState :: GameTree s mv -> s
-treeState (GameTree s _) = s
-
-edges :: GameTree s mv -> [Edge s mv]
-edges = e . treeNode
-  where e (Internal _ es) = es
-        e _               = []
-
-movesFrom :: GameTree s mv -> [mv]
-movesFrom = map fst . edges
-
-playerIx :: GameTree s mv -> Maybe PlayerID
-playerIx = ix . treeNode
-  where ix (Internal (Decision p) _) = Just p
-        ix _                         = Nothing
-
-
---
--- * Smart Constructors
---
-
--- | Decision node for a stateless game tree.
-decision :: PlayerID -> [Edge () mv] -> GameTree () mv
-decision p = GameTree () . Internal (Decision p)
-
--- | Chance node for a stateless game tree.
-chance :: Dist mv -> [Edge () mv] -> GameTree () mv
-chance d = GameTree () . Internal (Chance d)
-
--- | Payoff node for a stateless game tree.
-payoff :: [Float] -> GameTree () mv
-payoff = GameTree () . Payoff . fromList
-
--- | Build a stateless game tree in which multiple players choose in turn.
-decisions :: [(PlayerID,[mv])] -> ([mv] -> GameTree () mv) -> GameTree () mv
-decisions ps f = d ps []
-  where d []          ns = f (reverse ns)
-        d ((p,ms):ps) ns = decision p [(m, d ps (m:ns)) | m <- ms]
-
--- | Build a tree for a state-based game.
-stateGameTree :: (s -> PlayerID) -- ^ Whose turn is it?
-              -> (s -> Bool)     -- ^ Is the game over?
-              -> (s -> [mv])     -- ^ Available moves.
-              -> (s -> mv -> s)  -- ^ Execute a move and return the new state.
-              -> (s -> Payoff)   -- ^ Payoff for this (final) state.
-              -> s               -- ^ The current state.
-              -> GameTree s mv
-stateGameTree who end moves exec pay init = tree init
-  where tree s | end s     = GameTree s $ Payoff (pay s)
-               | otherwise = GameTree s $ Internal (Decision (who s)) [(m, tree (exec s m)) | m <- moves s]
-
---
--- * Constructing Payoffs
---
-
--- | The next player index out of n players.
-nextPlayer :: Int -> PlayerID -> PlayerID
-nextPlayer n p | p >= n    = 1
-               | otherwise = p + 1
-
--- | Payoff where all players out of n score payoff a, except player p, who scores b.
-allBut :: Int -> PlayerID -> Float -> Float -> Payoff
-allBut n p a b = ByPlayer $ replicate (p-1) a ++ b : replicate (n-p) a
-
--- | Add two payoffs.
-addPayoffs :: Payoff -> Payoff -> Payoff
-addPayoffs = dzipWith (+)
-
--- | Zero-sum payoff where player w wins (scoring n-1) 
---   and all other players lose (scoring -1).
-winner :: Int -> PlayerID -> Payoff
-winner n w = allBut n w (-1) (fromIntegral n - 1)
-
--- | Zero-sum payoff where player w loses (scoring -n+1)
---   and all other players, out of np, win (scoring 1).
-loser :: Int -> PlayerID -> Payoff
-loser n l = allBut n l 1 (1 - fromIntegral n)
-
--- | Zero-sum payoff where all players tie.  Each player scores 0.
-tie :: Int -> Payoff
-tie n = ByPlayer (replicate n 0)
+  -- | The available moves from a given node.
+  movesFrom :: g -> Node (State g) (Move g) -> [Move g]
--- Hagl/GameTree.hs
+++ Hagl/GameTree.hs
@@ -1,37 +1,120 @@
-{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE TypeFamilies, PatternGuards #-}
 
---
--- Functions for working with and traversing game trees.
---
+-- | Extensive form representation of games.
 module Hagl.GameTree where
 
-import Hagl.Base
+import Data.Maybe (fromMaybe)
 
--- The moves available from a node.
-movesFrom :: GameTree s mv -> [mv]
-movesFrom = map fst . edges
+import Hagl.Base.List
+import Hagl.Base.Payoff
+import Hagl.Base.Game
+
+--
+-- * Game Trees
+--
+
+-- | An edge represents a single transition from one location in a game tree
+--   to another, via a move.
+type Edge mv = (mv, GameTree mv)
+
+-- | A location in a game tree consists of an action to perform
+--   and a discrete set of outbound edges.
+data GameTree mv = GameTree {
+  action :: Action mv, -- ^ The action to perform at this location.
+  edges  :: [Edge mv]  -- ^ The outbound edges.
+} deriving Eq
+
+-- | Get the game tree corresponding to a discrete game.
+toGameTree :: DiscreteGame g => g -> GameTree (Move g)
+toGameTree g = build (start g)
+  where build n@(_,a) = GameTree a [(m, build (transition g n m)) | m <- movesFrom g n]
+
+
+--
+-- ** Instances
+--
+
+instance Eq mv => Game (GameTree mv) where
+  type State (GameTree mv) = [Edge mv]
+  type Move  (GameTree mv) = mv
+  start (GameTree a es) = (es,a)
+  transition _ (es,_) mv
+    | Just (GameTree a es) <- lookup mv es = (es,a)
+    | otherwise = error "GameTree: invalid move!"
+
+instance Eq mv => DiscreteGame (GameTree mv) where
+  movesFrom _ (es,_) = map fst es
+
+
+--
+-- ** Smart Constructors
+--
+
+-- | Decision node.
+decision :: PlayerID -> [Edge mv] -> GameTree mv
+decision = GameTree . Decision
+
+-- | Chance node.
+chance :: Dist mv -> [Edge mv] -> GameTree mv
+chance = GameTree . Chance
+
+-- | Payoff node.
+payoff :: [Float] -> GameTree mv
+payoff fs = GameTree (Payoff (fromList fs)) []
+
+-- | Begin a game tree in which multiple players decide in turn. The given
+--   function defines the branch resulting from each possible sequence of moves.
+decisions :: [(PlayerID,[mv])] -> ([mv] -> GameTree mv) -> GameTree mv
+decisions ps f = d [] ps
+  where d ms []         = f (reverse ms)
+        d ms ((p,l):ps) = decision p [(m, d (m:ms) ps) | m <- l]
+
+-- | Build a tree for a state-based game.
+stateGameTree :: (s -> PlayerID) -- ^ Whose turn is it?
+              -> (s -> Bool)     -- ^ Is the game over?
+              -> (s -> [mv])     -- ^ Available moves.
+              -> (s -> mv -> s)  -- ^ Execute a move and return the new state.
+              -> (s -> Payoff)   -- ^ Payoff for this (final) state.
+              -> s               -- ^ The current state.
+              -> GameTree mv
+stateGameTree who end moves exec pay init = tree init
+  where tree s | end s     = GameTree (Payoff (pay s)) []
+               | otherwise = GameTree (Decision (who s)) [(m, tree (exec s m)) | m <- moves s]
+
+
+--
+-- ** Simple Queries
+--
+
+-- | Get the PlayerID corresponding to a decision node.
+playerID :: GameTree mv -> Maybe PlayerID
+playerID (GameTree (Decision p) _) = Just p
+playerID _                         = Nothing
+
+-- The highest numbered player in this finite game tree.
+maxPlayer :: GameTree mv -> Int
+maxPlayer t = foldl1 max $ map (fromMaybe 0 . playerID) (dfs t)
 
 -- The immediate children of a node.
-children :: GameTree s mv -> [GameTree s mv]
+children :: GameTree mv -> [GameTree mv]
 children = map snd . edges
 
 -- Get a particular child node by following the edge labeled with mv.
-child :: Eq mv => mv -> GameTree s mv -> GameTree s mv
+child :: Eq mv => mv -> GameTree mv -> GameTree mv
 child mv t | Just t' <- lookup mv (edges t) = t'
 child _  _ = error "GameTree.child: invalid move"
 
+
+--
+-- ** Traversals
+--
+
 -- Nodes in BFS order.
-bfs :: GameTree s mv -> [GameTree s mv]
+bfs :: GameTree mv -> [GameTree mv]
 bfs t = bfs' [t]
   where bfs' [] = []
         bfs' ns = ns ++ bfs' (concatMap children ns)
 
 -- Nodes in DFS order.
-dfs :: GameTree s mv -> [GameTree s mv]
+dfs :: GameTree mv -> [GameTree mv]
 dfs t = t : concatMap dfs (children t)
-
--- The highest numbered player in this finite game tree.
-maxPlayer :: GameTree s mv -> Int
-maxPlayer t = foldl1 max $ map player (dfs t)
-  where player (GameTree _ (Internal (Decision p) _)) = p
-        player _                                      = 0
