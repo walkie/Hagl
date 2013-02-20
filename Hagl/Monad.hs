d2dda2a89461f92c34975ed5204d6c2b8f369055
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-19 21:32:23 -0800

    Update to reflect game graph representation, improved comments.

--- Hagl/Monad.hs
+++ Hagl/Monad.hs
@@ -3,59 +3,70 @@
              FunctionalDependencies,
              MultiParamTypeClasses #-}
 
--- This module contains stuff that almost every Hagl file will need to import.
-module Hagl.Base.Monad where
+-- | This module defines the game execution and player strategy monads.
+module Hagl.Monad where
 
 import Control.Monad.State hiding (State)
 import Data.Function (on)
 
-import Hagl.Base.List
-import Hagl.Base.Game
+import Hagl.Lists
+import Hagl.Game
 
 
----------------------
--- Execution State --
----------------------
+--
+-- * Execution State
+--
 
--- Transcripts
-type Moved mv       = (Maybe PlayerID, mv)
-type Transcript mv  = [Moved mv]
+-- | A record of a single move event.
+type MoveEvent mv   = (Maybe PlayerID, mv)
+
+-- | A transcript is a list of move events.
+type Transcript mv  = [MoveEvent mv]
+
+-- | A summary of all the moves made by each player during the game.
 type MoveSummary mv = ByPlayer (ByTurn mv)
 
-moved :: Decision mv -> mv -> Moved mv
-moved (Decision p) mv = (Just p,  mv)
-moved (Chance   _) mv = (Nothing, mv)
+-- | Create a move event from an action and the move made.
+moveEvent :: Action mv -> mv -> MoveEvent mv
+moveEvent (Decision p) mv = (Just p,  mv)
+moveEvent (Chance   _) mv = (Nothing, mv)
 
+-- | Produce a move summary from a transcript.
 summarize :: Int -> Transcript mv -> MoveSummary mv
 summarize np t = ByPlayer [ByTurn [mv | (mi,mv) <- t, mi == Just p] | p <- [1..np]]
 
--- Game Execution State
+-- | The state of a game execution.
 data Exec g = Exec {
-  _game        :: g,                           -- ^ Game definition.
-  _players     :: ByPlayer (Player g),         -- ^ Players active in the game.
-  _location    :: GameTree (State g) (Move g), -- ^ The current location in the game tree.
-  _numMoves    :: ByPlayer Int,                -- ^ Number of moves played by each player
-  _transcript  :: Transcript (Move g),         -- ^ Moves played so far (newest at head)
-  _finalPayoff :: Maybe Payoff                 -- ^ Final payoff, once the game is completed
+  _game        :: g,                       -- ^ Game definition.
+  _players     :: ByPlayer (Player g),     -- ^ Players active in the game.
+  _location    :: Node (State g) (Move g), -- ^ The current location in the game graph.
+  _numMoves    :: ByPlayer Int,            -- ^ Number of moves played by each player
+  _transcript  :: Transcript (Move g),     -- ^ Moves played so far (newest at head)
+  _finalPayoff :: Maybe Payoff             -- ^ Final payoff, once the game is completed
 }
 
+-- | Initial game execution state.
 initExec :: Game g => g -> [Player g] -> Exec g
-initExec g ps = Exec g (ByPlayer ps) (gameTree g np) ms [] Nothing
+initExec g ps = Exec g (ByPlayer ps) (start g) ms [] Nothing
   where ms = ByPlayer (replicate np 0)
         np = length ps
 
 
--------------
--- Players --
--------------
+--
+-- * Players
+--
 
+-- | The name of the player.
 type Name = String
 
+-- | Each player has a name and strategy, and may optionally maintain
+--   their own personal state.
 data Player g = forall s. Player Name s (Strategy s g)
               | Name ::: Strategy () g
 
 infix 0 :::
 
+-- | Get the name of a player.
 name :: Player g -> Name
 name (Player n _ _) = n
 name (n ::: _)      = n
@@ -68,14 +79,25 @@ instance Ord (Player g) where
   compare = compare `on` name
 
 
------------------------------------
--- Execution and Strategy Monads --
------------------------------------
+--
+-- * Execution and Strategy Monads
+--
 
-data ExecM      g a = ExecM  { unE :: StateT (Exec g) IO a }
-data StratM   s g a = StratM { unS :: StateT s (ExecM g) a }
-type Strategy s g   = StratM s g (Move g)
+-- | The game execution monad.  A state monad transformer that maintains the
+--   game execution state.
+data ExecM g a = ExecM  { unE :: StateT (Exec g) IO a }
 
+-- | The strategy monad.  A state monad transformer that maintains the personal
+--   state of the player who is playing this strategy, and wraps the game
+--   execution monad.  This gives strategies access to the game execution state.
+data StratM s g a = StratM { unS :: StateT s (ExecM g) a }
+
+-- | A strategy is a computation in the strategy monad that produces a move.
+type Strategy s g = StratM s g (Move g)
+
+-- | Similar to MonadIO for monads that wrap IO, this type class captures all
+--   monads that wrap the game execution monad, providing uniform access to the
+--   game execution state.
 class (Game g, Monad m, MonadIO m) => GameM m g | m -> g where
   getExec :: m (Exec g)
 
@@ -89,6 +111,8 @@ evalGame g ps f = evalStateT (unE f) (initExec g ps)
 execGame :: Game g => g -> [Player g] -> ExecM g a -> IO (Exec g)
 execGame g ps f = execStateT (unE f) (initExec g ps)
 
+-- | Run the strategy associated with a particular player, producing a move
+--   and an updated player state.
 runStrategy :: Player g -> ExecM g (Move g, Player g)
 runStrategy p@(n ::: m) = do 
     mv <- evalStateT (unS m) ()
@@ -98,9 +122,9 @@ runStrategy (Player n s f) = do
     return (mv, Player n s' f)
 
 
----------------
--- Instances --
----------------
+--
+-- * Instances
+--
 
 instance Monad (ExecM g) where
   return = ExecM . return
