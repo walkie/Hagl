e8d6abbd405275ab76fa5fc4173dd3838d574618
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-20 17:10:21 -0800

    Ported Hagl.Iterated.List.

--- Hagl/Iterated.hs
+++ Hagl/Iterated.hs
@@ -1,13 +1,68 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module Hagl.Iterated.Game where
+module Hagl.Iterated where
 
 import Data.Maybe (fromMaybe)
 import Data.List  (transpose)
 
-import Hagl.Base
-import Hagl.Iterated.List
+import Hagl.Lists
+import Hagl.Game
+import Hagl.GameTree
+import Hagl.Exec
 
+--
+-- * ByGame lists
+--
+
+-- | A list where each element corresponds to one played iteration of
+--   an iterated game.
+newtype ByGame a = ByGame [a] deriving (Eq,Show,Functor)
+
+-- | Return the element corresponding to the given iteration number.
+forGame :: Int -> ByGame a -> a
+forGame i (ByGame as) = as !! (length as - i)
+
+-- | Return the elements corresponding to every iteration (all elements as a
+--   plain list).
+everyGame :: ByGame a -> [a]
+everyGame (ByGame as) = as
+
+-- | Return the elements corresponding to all completed iterations.
+completedGames :: ByGame a -> [a]
+completedGames (ByGame []) = error "completedGames: Empty game list."
+completedGames (ByGame as) = tail as
+
+-- | Return the element corresponding to the first iteration.
+firstGame :: ByGame a -> a
+firstGame (ByGame []) = error "firstGame: Empty game list."
+firstGame (ByGame as) = last as
+
+-- | Return the element corresponding to the current iteration.
+thisGame :: ByGame a -> a
+thisGame (ByGame []) = error "thisGame: Empty game list."
+thisGame (ByGame as) = head as
+
+-- | Return the element corresponding to the most recently completed iteration.
+lastGame :: ByGame a -> a
+lastGame (ByGame [])      = error "lastGame: Empty game list."
+lastGame (ByGame [a])     = error "lastGame: No completed games."
+lastGame (ByGame (_:a:_)) = a
+
+-- | Return the elements corresponding to the most recently completed n
+--   iterations of the game.
+lastNGames :: Int -> ByGame a -> [a]
+lastNGames _ (ByGame []) = error "lastNGames: Empty game list."
+lastNGames i (ByGame (_:as))
+    | length as' == i = as'
+    | otherwise       = error "lastNGames: Not enough games."
+  where as' = take i as
+
+-- Instances
+
+instance ByX ByGame where
+  toAssocList (ByGame l) = zip [length l ..] l
+  minX = firstGame
+  maxX = thisGame
 
 --------------------
 -- Representation --
