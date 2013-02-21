216ad197fd2711cf4c45568e22fc011e7282beda
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-20 23:30:12 -0800

    Top-level module.

--- Hagl.hs
+++ Hagl.hs
@@ -1,23 +1,20 @@
--- This module provides a convenient way to import most of Hagl all at once.
--- Modules relating to particular types of games should be imported separately,
--- since they contain overlapping function names.  Game-specific modules include
--- Hagl.Extensive, Hagl.Normal, and Hagl.StateBased.
+-- | This module provides a convenient way to import all of Hagl at once.
 module Hagl (
-  module Hagl.Accessor,
-  module Hagl.Core,
   module Hagl.Exec,
   module Hagl.Game,
   module Hagl.GameTree,
-  module Hagl.Print,
-  module Hagl.Selector,
-  module Hagl.Strategy
+  module Hagl.Iterated,
+  module Hagl.Lists,
+  module Hagl.Normal,
+  module Hagl.Strategy,
+  module Hagl.Tournament
 ) where
 
-import Hagl.Accessor
-import Hagl.Core
 import Hagl.Exec
 import Hagl.Game
 import Hagl.GameTree
-import Hagl.Print
-import Hagl.Selector
+import Hagl.Iterated
+import Hagl.Lists
+import Hagl.Normal
 import Hagl.Strategy
+import Hagl.Tournament
