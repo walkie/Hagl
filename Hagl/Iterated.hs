d35e2b6e223835a536a213eac62880335a8b2879
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-20 21:20:53 -0800

    Reordering.

--- Hagl/Iterated.hs
+++ Hagl/Iterated.hs
@@ -11,6 +11,122 @@ import Hagl.Game
 import Hagl.GameTree
 import Hagl.Exec
 
+
+--
+-- * Representation
+--
+
+-- | An iterated game can have either a finite or infinite number of iterations.
+data Limit = Finite Int | Infinite deriving Eq
+
+-- | Representation of iterated games.
+data Iterated g = Iterated Limit g
+
+-- | Construct an infinitely iterated game from a non-iterated game.
+iterated :: g -> Iterated g
+iterated = Iterated Infinite
+
+-- | Get the uniterated form of a game.
+uniterated :: Iterated g -> g
+uniterated (Iterated _ g) = g
+
+-- | Get the limit of an iterated game.
+limit :: Iterated g -> Limit
+limit (Iterated l _) = l
+
+-- | `Just n` if the limit is finite, `Nothing` otherwise.
+limitToMaybe :: Limit -> Maybe Int
+limitToMaybe (Finite i) = Just i
+limitToMaybe Infinite   = Nothing
+
+-- | Given a number of iterations, has the limit been reached?
+reached :: Int -> Limit -> Bool
+reached _ Infinite   = False
+reached a (Finite b) = a < b
+
+-- Show instances
+--
+
+instance Show Limit where
+  show (Finite n) = "Iterated " ++ show n ++ " times"
+  show Infinite   = "Iterated"
+
+instance Show g => Show (Iterated g) where
+  show (Iterated l g) = "(" ++ show l ++ ")\n" ++ show g
+
+
+--
+-- * Iterated game execution state
+--
+
+-- | Summary of each iteration: a summary of moves by each player, and
+--   a payoff if the game is complete.
+type Summary mv = (MoveSummary mv, Maybe Payoff)
+
+-- | The execution history of an iterated game: a transcript and summary
+--   of each completed game.
+type History mv = ByGame (Transcript mv, Summary mv)
+
+-- | Iterated game execution state.
+data Iter s mv = Iter {
+  _gameNumber     :: Int,           -- ^ The current iteration number.
+  _history        :: History mv,    -- ^ History of all completed game iterations.
+  _gameTranscript :: Transcript mv, -- ^ The transcript of the current iteration.
+  _gameState      :: s              -- ^ The state of the current game iteration.
+}
+
+-- | Initial iterated game execution state.
+initIter :: s -> Iter s mv
+initIter = Iter 1 (ByGame []) []
+
+-- | Get the transripts from a history.
+_transcripts :: History mv -> ByGame (Transcript mv)
+_transcripts = fmap fst
+
+-- | Get the iteration summaries from a history.
+_summaries :: History mv -> ByGame (Summary mv)
+_summaries = fmap snd
+
+-- | Get the move summaries from an iteration summary.
+_moveSummary :: Summary mv -> MoveSummary mv
+_moveSummary = fst
+
+-- | Get the payoff from the summary of a completed game.
+_payoff :: Summary mv -> Payoff
+_payoff = fromMaybe e . snd
+  where e = error "Incomplete game does not have a payoff!"
+
+-- | Compute the current score from a history.
+_score :: History mv -> Payoff
+_score = ByPlayer . map sum . transpose .  -- calculate score
+         map everyPlayer . everyGame .     -- convert to plain lists
+         fmap _payoff . _summaries         -- get payoffs for each game
+
+
+-- Game instances
+--
+
+instance Game g => Game (Iterated g) where
+  
+  type Move  (Iterated g) = Move g
+  type State (Iterated g) = Iter (State g) (Move g)
+  
+  start (Iterated _ g) = (initIter s, a)
+    where (s,a) = start g
+
+  transition (Iterated l g) (Iter n h t s, a) m =
+      case transition g (s,a) m of
+        (s',Payoff p) | reached n l -> (Iter n h' [] s', Payoff (_score h'))
+                      | otherwise   -> (Iter (n+1) h' [] (startState g), startAction g)
+          where np = length (toAssocList p)
+                h' = addForNewGame (t', (summarize np t', Just p)) h
+        (s',a') -> (Iter n h t' s', a')
+    where t' = moveEvent a m : t
+
+instance DiscreteGame g => DiscreteGame (Iterated g) where
+  movesFrom (Iterated _ g) (Iter _ _ _ s, a) = movesFrom g (s,a)
+
+
 --
 -- * ByGame lists
 --
@@ -62,8 +178,6 @@ lastNGames i (ByGame (_:as))
     | otherwise       = error "lastNGames: Not enough games."
   where as' = take i as
 
--- Instances
-
 instance ByX ByGame where
   toAssocList (ByGame l) = zip [length l ..] l
   minX = firstGame
@@ -96,116 +210,3 @@ lastGame's = liftM lastGame
 -- | Selects the elements corresponding to the last `n` completed iterations of the game.
 lastNGames' :: GameM m g => Int -> m (ByGame a) -> m [a]
 lastNGames' i = liftM (lastNGames i)
-
-
---
--- * Representation
---
-
--- | An iterated game can have either a finite or infinite number of iterations.
-data Limit = Finite Int | Infinite deriving Eq
-
--- | Representation of iterated games.
-data Iterated g = Iterated Limit g
-
--- | Construct an infinitely iterated game from a non-iterated game.
-iterated :: g -> Iterated g
-iterated = Iterated Infinite
-
--- | Get the uniterated form of a game.
-uniterated :: Iterated g -> g
-uniterated (Iterated _ g) = g
-
--- | Get the limit of an iterated game.
-limit :: Iterated g -> Limit
-limit (Iterated l _) = l
-
--- | `Just n` if the limit is finite, `Nothing` otherwise.
-limitToMaybe :: Limit -> Maybe Int
-limitToMaybe (Finite i) = Just i
-limitToMaybe Infinite   = Nothing
-
--- | Given a number of iterations, has the limit been reached?
-reached :: Int -> Limit -> Bool
-reached _ Infinite   = False
-reached a (Finite b) = a < b
-
-
---
--- Iterated game execution state
---
-
--- | Summary of each iteration: a summary of moves by each player, and
---   a payoff if the game is complete.
-type Summary mv = (MoveSummary mv, Maybe Payoff)
-
--- | The execution history of an iterated game: a transcript and summary
---   of each completed game.
-type History mv = ByGame (Transcript mv, Summary mv)
-
--- | Iterated game execution state.
-data Iter s mv = Iter {
-  _gameNumber     :: Int,           -- ^ The current iteration number.
-  _history        :: History mv,    -- ^ History of all completed game iterations.
-  _gameTranscript :: Transcript mv, -- ^ The transcript of the current iteration.
-  _gameState      :: s              -- ^ The state of the current game iteration.
-}
-
--- | Initial iterated game execution state.
-initIter :: s -> Iter s mv
-initIter = Iter 1 (ByGame []) []
-
--- | Get the transripts from a history.
-_transcripts :: History mv -> ByGame (Transcript mv)
-_transcripts = fmap fst
-
--- | Get the iteration summaries from a history.
-_summaries :: History mv -> ByGame (Summary mv)
-_summaries = fmap snd
-
--- | Get the move summaries from an iteration summary.
-_moveSummary :: Summary mv -> MoveSummary mv
-_moveSummary = fst
-
--- | Get the payoff from the summary of a completed game.
-_payoff :: Summary mv -> Payoff
-_payoff = fromMaybe e . snd
-  where e = error "Incomplete game does not have a payoff!"
-
--- | Compute the current score from a history.
-_score :: History mv -> Payoff
-_score = ByPlayer . map sum . transpose .  -- calculate score
-         map everyPlayer . everyGame .     -- convert to plain lists
-         fmap _payoff . _summaries         -- get payoffs for each game
-
-
---
--- Instances
---
-
-instance Game g => Game (Iterated g) where
-  
-  type Move  (Iterated g) = Move g
-  type State (Iterated g) = Iter (State g) (Move g)
-  
-  start (Iterated _ g) = (initIter s, a)
-    where (s,a) = start g
-
-  transition (Iterated l g) (Iter n h t s, a) m =
-      case transition g (s,a) m of
-        (s',Payoff p) | reached n l -> (Iter n h' [] s', Payoff (_score h'))
-                      | otherwise   -> (Iter (n+1) h' [] (startState g), startAction g)
-          where np = length (toAssocList p)
-                h' = addForNewGame (t', (summarize np t', Just p)) h
-        (s',a') -> (Iter n h t' s', a')
-    where t' = moveEvent a m : t
-
-instance DiscreteGame g => DiscreteGame (Iterated g) where
-  movesFrom (Iterated _ g) (Iter _ _ _ s, a) = movesFrom g (s,a)
-
-instance Show Limit where
-  show (Finite n) = "Iterated " ++ show n ++ " times"
-  show Infinite   = "Iterated"
-
-instance Show g => Show (Iterated g) where
-  show (Iterated l g) = "(" ++ show l ++ ")\n" ++ show g
