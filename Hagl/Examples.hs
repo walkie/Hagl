1811d286b09e5174a5afabcc91ff3e813fac679b
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-22 05:27:30 -0800

    New module to export all examples.

---
+++ Hagl/Examples.hs
@@ -0,0 +1,12 @@
+-- | This module provides a convenient way to import all of the examples at once.
+module Hagl.Examples (
+  module Hagl.Examples.Crisis,
+  module Hagl.Examples.Matrix,
+  module Hagl.Examples.Prisoner,
+  module Hagl.Examples.RPS
+) where
+
+import Hagl.Examples.Crisis
+import Hagl.Examples.Matrix
+import Hagl.Examples.Prisoner
+import Hagl.Examples.RPS
