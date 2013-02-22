3ab66add5c11dd883b8f1ac4b145939a46afff2f
Eric Walkingshaw <eric@walkingshaw.net>
2013-02-22 04:19:41 -0800

    Add types and haddock comments.

--- Examples/Prisoner.hs
+++ Examples/Prisoner.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleInstances,
+             NoMonomorphismRestriction,
+             TypeFamilies,
+             UndecidableInstances #-}
 
 {- |
 
@@ -10,7 +13,7 @@ From GHCi, try some of the following.
 > pareto pd
 > paretoNash pd
 > paretoNash stag
-> execGame (iterated pd) [tft, pavlov] (times 10 >> printTranscripts >> printScore)
+> execGame ipd [tft, pavlov] (times 10 >> printTranscripts >> printScore)
 > axelrod [fink, tft, grim', pavlov, preserver]
 
 -}
@@ -22,59 +25,129 @@ import Prelude hiding (last, print)
 
 import Hagl
 
-------------------------
--- Prisoner's Dilemma --
-------------------------
+--
+-- * Game representations
+--
 
--- Game definition
-data Cooperation = C | D deriving (Eq, Read, Show)
+-- | A move indicating whether to cooperate or not.
+data Cooperation = C -- ^ Cooperate
+                 | D -- ^ Defect
+  deriving (Eq, Read, Show)
+
+-- | A dilemma game is a normal form game about cooperating or defecting.
 type Dilemma = Normal Cooperation
 
-pd   = symmetric [C,D] [2,0,3,1]
+-- | The classic prisoner's dilemma.
+pd :: Dilemma
+pd = symmetric [C,D] [2,0,3,1]
+
+-- | Iterated prisoner's dilemma.
+ipd :: Iterated Dilemma
+ipd = iterated pd
+
+-- | The stag hunt. Similar to the prisoner's dilemma except mutual
+--   cooperation is both a Nash equilibrium and Pareto optimal, and is
+--   therefore stable.
+stag :: Dilemma
 stag = symmetric [C,D] [3,0,2,1]
 
--- Some simple players.
+-- | Iterated stag hunt.
+istag :: Iterated Dilemma
+istag = iterated stag
+
+-- | Captures both iterated and un-iterated dilemma games.
+--   (Mostly just to improve type annotations.)
+class (Game g, Move g ~ Cooperation) => DilemmaGame g
+instance DilemmaGame Dilemma
+instance DilemmaGame (Iterated Dilemma)
+
+
+--
+-- * Players
+--
+
+--
+-- ** Simple strategies
+
+-- | Always defects.
+fink :: DilemmaGame g => Player g
 fink = "Fink" ::: pure D
+
+-- | Always cooperates.
+mum :: DilemmaGame g => Player g
 mum = "Mum" ::: pure C
+
+-- | Alternates between cooperation and defection.
+alt :: DilemmaGame g => Player g
 alt = "Alternator" ::: periodic [C,D]
+
+-- | Alternates between defection and cooperation.
+dc :: DilemmaGame g => Player g
 dc = "(DC)*" ::: periodic [D,C]
+
+-- | Defects every third round.
+ccd :: DilemmaGame g => Player g
 ccd = "(CCD)*" ::: periodic [C,C,D]
-randy = "Randy" ::: randomly
+
+-- | Defects randomly with a probability of 1/6.
+rr :: DilemmaGame g => Player g
 rr = "Russian Roulette" ::: mixed [(5,C), (1,D)]
 
--- The famous Tit-for-Tat.
+-- | Plays randomly, cooperates or defects with equal probability.
+randy :: DiscreteGame g => Player g
+randy = "Randy" ::: randomly
+
+
+--
+-- ** More sophisticated/complex strategies
+
+-- | The famous Tit-for-Tat. Cooperates initially, then plays the last
+--   move played by its opponent.
+tft :: Player (Iterated Dilemma)
 tft = "Tit for Tat" ::: play C `atFirstThen` his (lastGame's onlyMove)
 
-stately = Player "Stately Alternator" C $
+-- | The same strategy as 'alt', implemented using state.
+alt' :: DilemmaGame g => Player g
+alt' = Player "Stately Alternator" C $
   do m <- get
      put $ if m == C then D else C
      return m
 
+-- | Cooperates every third round, implemented using state.
+mod3 :: DilemmaGame g => Player g
 mod3 = Player "Mod3 Cooperator" 0 $
   do i <- get
      put (i+1)
      return $ if i `mod` 3 == 0 then C else D
      
--- Suspicious Tit-for-Tat (like Tit-for-Tat but defect on first move)
+-- | Suspicious Tit-for-Tat.  Like Tit-for-Tat but defect on first move.
+suspicious :: Player (Iterated Dilemma)
 suspicious = "Suspicious Tit-for-Tat" ::: play D `atFirstThen` his (lastGame's onlyMove)
 
--- Tit-for-Tat that only defects after two defects in a row.
+-- | A variant of Tit-for-Tat that only defects after two defects in a row.
+titForTwoTats :: Player (Iterated Dilemma)
 titForTwoTats = "Tit-for-Two-Tats" :::
     do ms <- his `each` lastNGames' 2 onlyMove
        return $ if ms == [D, D] then D else C
 
--- The Grim Trigger: Cs until opponent defects, then defects forever.
+-- | The Grim Trigger.  Cooperates until the opponent defects, then defects
+--   forever.
+grim :: Player (Iterated Dilemma)
 grim = "Grim Trigger" :::
     do ms <- his `each` completedGames' onlyMove
        if D `elem` ms then play D else play C
 
+-- | The Grim Trigger, implemented using state.  Much faster than 'grim', since
+--   it doesn't examine every previous game iteration.
+grim' :: Player (Iterated Dilemma)
 grim' = Player "Stately Grim" False $ 
   play C `atFirstThen`
   do m <- her (lastGame's onlyMove)
      triggered <- update (|| m == D)
      if triggered then play D else play C
 
--- If last move resulted in a "big" payoff, do it again, otherwise switch.
+-- | If last move resulted in a "big" payoff, do it again, otherwise switch.
+pavlov :: Player (Iterated Dilemma)
 pavlov = "Pavlov" :::
     randomly `atFirstThen`
     do p <- my (lastGame's payoffs)
@@ -82,14 +155,20 @@ pavlov = "Pavlov" :::
        return $ if p > 1 then m else
                 if m == C then D else C
 
--- Made-up strategy: Pick randomlyly until we have a lead, then
--- preserve it by repeatedly choosing D.
+-- | Picks randomly until it has a lead, then preserves it by repeatedly
+--   defecting.
+preserver :: Player (Iterated Dilemma)
 preserver = "Preserver" :::
     randomly `atFirstThen`
     do me <- my score
        he <- his score
        if me > he then return D else randomly
 
--- Run an Axelrod-style tournament.
+
+--
+-- * Experiments
+--
+
+-- | Run a tournament similar to Robert Axelrod's famous study.
 axelrod :: [Player (Iterated Dilemma)] -> IO ()
-axelrod ps = roundRobin (iterated pd) 2 ps (times 200) >>= printResults
+axelrod ps = roundRobin ipd 2 ps (times 200) >>= printResults
