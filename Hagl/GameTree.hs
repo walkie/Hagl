787a53bec156502fced27c23bb0b61f75dc50302
Eric Walkingshaw <eric@walkingshaw.net>
2013-03-04 18:16:12 -0800

    Distinguish discrete and continuous trees.

--- Hagl/GameTree.hs
+++ Hagl/GameTree.hs
@@ -15,47 +15,60 @@ import Hagl.Game
 -- * Representation
 --
 
+-- | A location in a game tree consists of a node and some representation of
+--   outbound edges.
+class GameTree t where
+  -- | The state and action of this location.
+  treeNode :: t s mv -> Node s mv
+  -- | An implicit representation of the outbound edges.
+  treeMove :: Eq mv => t s mv -> mv -> Maybe (t s mv)
+
+-- | The state at a given tree node.
+treeState :: GameTree t => t s mv -> s
+treeState = nodeState . treeNode
+
+-- | The action at a given tree node.
+treeAction :: GameTree t => t s mv -> Action mv
+treeAction = nodeAction . treeNode
+
 -- | An edge represents a single transition from one location in a game tree
 --   to another, via a move.
-type Edge s mv = (mv, GameTree s mv)
+type Edge s mv = (mv, Discrete s mv)
 
 -- | The move assicated with an edge.
 edgeMove :: Edge s mv -> mv
 edgeMove = fst
 
 -- | The destination of an edge.
-edgeDest :: Edge s mv -> GameTree s mv
+edgeDest :: Edge s mv -> Discrete s mv
 edgeDest = snd
 
-
--- | A location in a game tree consists of an action to perform
---   and a discrete set of outbound edges.
-data GameTree s mv = GameTree {
-  treeNode  :: Node s mv,   -- ^ The game state and action associated with this location.
-  treeEdges :: [Edge s mv]  -- ^ The outbound edges.
+-- | A discrete game tree provides a finite list of outbound edges from
+--   each location, providing a discrete and finite list of available moves.
+data Discrete s mv = Discrete {
+  -- | The game state and action associated with this location.
+  dtreeNode  :: Node s mv,
+  -- | The outbound edges.
+  dtreeEdges :: [Edge s mv]
 } deriving Eq
 
--- | The state at a given tree node.
-treeState :: GameTree s mv -> s
-treeState = nodeState . treeNode
+-- | The edges of a continuous game tree are defined by a transition function
+--   from moves to children, supporting a potentially infinite or continuous
+--   set of available moves.
+data Continuous s mv = Continuous {
+  -- | The game state and action associated with this location.
+  ctreeNode :: Node s mv,
+  -- | The transition function.
+  ctreeMove :: mv -> Maybe (Continuous s mv)
+}
 
--- | The action at a given tree node.
-treeAction :: GameTree s mv -> Action mv
-treeAction = nodeAction . treeNode
+instance GameTree Discrete where
+  treeNode = dtreeNode
+  treeMove t m = lookup m (dtreeEdges t)
 
-
--- Instances
-
-instance Eq mv => Game (GameTree s mv) where
-  type State (GameTree s mv) = (s,[Edge s mv])
-  type Move  (GameTree s mv) = mv
-  start (GameTree (s,a) es) = ((s,es),a)
-  transition _ ((_,es),_) mv
-    | Just (GameTree (s,a) es') <- lookup mv es = ((s,es'),a)
-    | otherwise = error "GameTree: invalid move!"
-
-instance Eq mv => DiscreteGame (GameTree s mv) where
-  movesFrom _ ((_,es),_) = map edgeMove es
+instance GameTree Continuous where
+  treeNode = ctreeNode
+  treeMove = ctreeMove
 
 
 --
@@ -63,12 +76,12 @@ instance Eq mv => DiscreteGame (GameTree s mv) where
 --
 
 -- | Generate the game tree for a discrete game.
-gameTree :: DiscreteGame g => g -> GameTree (State g) (Move g)
+gameTree :: DiscreteGame g => g -> Discrete (State g) (Move g)
 gameTree g = gameTreeFrom g (start g)
 
 -- | Generate the game tree from a particular location in a discrete game.
-gameTreeFrom :: DiscreteGame g => g -> Node (State g) (Move g) -> GameTree (State g) (Move g)
-gameTreeFrom g n = GameTree n [(m, gameTreeFrom g (transition g n m)) | m <- movesFrom g n]
+gameTreeFrom :: DiscreteGame g => g -> Node (State g) (Move g) -> Discrete (State g) (Move g)
+gameTreeFrom g n = Discrete n [(m, gameTreeFrom g (transition g n m)) | m <- movesFrom g n]
 
 -- | Build a tree for a state-based game.
 stateGameTree :: (s -> PlayerID) -- ^ Whose turn is it?
@@ -77,10 +90,10 @@ stateGameTree :: (s -> PlayerID) -- ^ Whose turn is it?
               -> (s -> mv -> s)  -- ^ Execute a move and return the new state.
               -> (s -> Payoff)   -- ^ Payoff for this (final) state.
               -> s               -- ^ The current state.
-              -> GameTree s mv
+              -> Discrete s mv
 stateGameTree who end moves exec pay = tree
-  where tree s | end s     = GameTree (s, Payoff (pay s)) []
-               | otherwise = GameTree (s, Decision (who s)) [(m, tree (exec s m)) | m <- moves s]
+  where tree s | end s     = Discrete (s, Payoff (pay s)) []
+               | otherwise = Discrete (s, Decision (who s)) [(m, tree (exec s m)) | m <- moves s]
 
 
 --
@@ -88,21 +101,22 @@ stateGameTree who end moves exec pay = tree
 --
 
 -- | Get the PlayerID corresponding to a decision node.
-playerID :: GameTree s mv -> Maybe PlayerID
-playerID (GameTree (_, Decision p) _) = Just p
-playerID _                            = Nothing
+playerID :: GameTree t => t s mv -> Maybe PlayerID
+playerID t = case treeAction t of
+               Decision p -> Just p
+               _          -> Nothing
 
 -- | The highest numbered player in this finite game tree.
-maxPlayer :: GameTree s mv -> Int
+maxPlayer :: Discrete s mv -> Int
 maxPlayer t = maximum $ map (fromMaybe 0 . playerID) (dfs t)
 
 -- | The immediate children of a node.
-children :: GameTree s mv -> [GameTree s mv]
-children = map edgeDest . treeEdges
+children :: Discrete s mv -> [Discrete s mv]
+children = map edgeDest . dtreeEdges
 
 -- | Get a particular child node by following the edge labeled with mv.
-child :: Eq mv => mv -> GameTree s mv -> GameTree s mv
-child mv t | Just t' <- lookup mv (treeEdges t) = t'
+child :: Eq mv => mv -> Discrete s mv -> Discrete s mv
+child mv t | Just t' <- lookup mv (dtreeEdges t) = t'
 child _  _ = error "GameTree.child: invalid move"
 
 
@@ -111,13 +125,13 @@ child _  _ = error "GameTree.child: invalid move"
 --
 
 -- | Nodes in BFS order.
-bfs :: GameTree s mv -> [GameTree s mv]
+bfs :: Discrete s mv -> [Discrete s mv]
 bfs t = bfs' [t]
   where bfs' [] = []
         bfs' ns = ns ++ bfs' (concatMap children ns)
 
 -- | Nodes in DFS order.
-dfs :: GameTree s mv -> [GameTree s mv]
+dfs :: Discrete s mv -> [Discrete s mv]
 dfs t = t : concatMap dfs (children t)
 
 
@@ -126,13 +140,13 @@ dfs t = t : concatMap dfs (children t)
 --
 
 -- | A nice string representation of a game tree.
-drawTree :: Show mv => GameTree s mv -> String
+drawTree :: Show mv => Discrete s mv -> String
 drawTree = condense . DT.drawTree . tree ""
   where
     condense = unlines . filter empty . lines
     empty    = not . all (\c -> c == ' ' || c == '|')
-    tree s t@(GameTree (_,a) _) = DT.Node (s ++ show a)
-                                  [tree (show m ++ " -> ") t | (m,t) <- treeEdges t]
+    tree s t@(Discrete (_,a) _) = DT.Node (s ++ show a)
+                                  [tree (show m ++ " -> ") t | (m,t) <- dtreeEdges t]
 
-instance Show mv => Show (GameTree s mv) where
+instance Show mv => Show (Discrete s mv) where
   show = drawTree
