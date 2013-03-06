6d42d91456496c5c240b290e5129b8a3d162a880
Eric Walkingshaw <eric@walkingshaw.net>
2013-03-05 19:22:31 -0800

    Ported Tic Tac Toe.

--- Hagl/Examples.hs
+++ Hagl/Examples.hs
@@ -1,11 +1,11 @@
 -- | This module provides a convenient way to import all of the examples at once.
 module Hagl.Examples (module X) where
 
-import Hagl                   as X
-import Hagl.Examples.Auction  as X
-import Hagl.Examples.Chance   as X
-import Hagl.Examples.Crisis   as X
-import Hagl.Examples.Matrix   as X
-import Hagl.Examples.Prisoner as X
-import Hagl.Examples.RPS      as X hiding (randy)
-
+import Hagl                    as X
+import Hagl.Examples.Auction   as X
+import Hagl.Examples.Chance    as X
+import Hagl.Examples.Crisis    as X
+import Hagl.Examples.Matrix    as X
+import Hagl.Examples.Prisoner  as X
+import Hagl.Examples.RPS       as X hiding (randy)
+import Hagl.Examples.TicTacToe as X
--- Hagl/Examples/TicTacToe.hs
+++ Hagl/Examples/TicTacToe.hs
@@ -1,81 +1,90 @@
 {-# LANGUAGE FlexibleContexts, TypeFamilies #-}
-module Examples.TicTacToe where
+
+-- | An implementation of the game Tic Tac Toe.
+--   Try playing against the minimax algorithm by running in GHCi:
+--
+--   > playTicTacToe
+--
+--   Valid moves are the integers [0..8], where each integer names square of
+--   the board, starting in the upper left and proceeding in the order that 
+--   you would read a book.  The named square must be empty.
+module Hagl.Examples.TicTacToe where
 
 import Control.Monad.Trans (liftIO)
 import Data.List           (elemIndices, intersperse, transpose)
 import Text.Printf         (printf)
 
 import Hagl
-import Hagl.Searchable
 
------------------
--- Tic Tac Toe --
------------------
+--
+-- * Game represetation.
+--
 
+-- | Each square either has an X, an O, or is empty.
 data Square = X | O | E deriving Eq
-type Board = [Square]
-type Mark  = Int
 
+-- | A board is a 3x3 grid of squares, represented as a 9-element list.
+type Board = [Square]
+
+-- | A move is indicated by a naming the index of the square to put your
+--   mark in, [0..8].
+type Mark = Int
+
+-- | A trivial data type for Tic Tac Toe.
 data TicTacToe = TicTacToe
 
--- The initial board.
+-- | The initial board.
 start :: Board
 start = replicate 9 E
 
--- The mark corresponding to each player.
+-- | The mark corresponding to each player.
 xo 1 = X
 xo 2 = O
 
--- Get a list of empty squares.
+-- | Get a list of empty squares.
 markable :: Board -> [Mark]
 markable = elemIndices E
 
--- The player whose turn it is.
-who :: Board -> PlayerIx
+-- | The player whose turn it is.
+who :: Board -> PlayerID
 who b = if (odd . length . markable) b then 1 else 2
 
--- Player p marks square m.
-mark :: PlayerIx -> Mark -> Board -> Board
-mark p m b = take m b ++ xo p : drop (m+1) b
+-- | Player p marks square m.
+mark :: Board -> Mark -> Board
+mark b m = take m b ++ xo (who b) : drop (m+1) b
 
--- True if player p has won.
-win :: PlayerIx -> Board -> Bool
+-- | True if player p has won.
+win :: PlayerID -> Board -> Bool
 win p b = any (all (xo p ==)) (h ++ v ++ d)
   where h = chunk 3 b
         v = transpose h
         d = map (map (b !!)) [[0,4,8],[2,4,6]]
 
--- True if the game is over.
+-- | True if the game is over.
 end :: Board -> Bool
 end b = null (markable b) || win 1 b || win 2 b
 
--- The payoff awarded for a final state.
+-- | The payoff awarded for a final state.
 pay :: Board -> Payoff
 pay b | win 1 b   = winner 2 1
       | win 2 b   = winner 2 2
       | otherwise = tie 2
 
--- Play a game against minimax!
-challenge = execGame TicTacToe ["Puny Human" ::: human, "Minimax" ::: minimax]
-    (run >> printScore)
-  where run = printGame >> step >>= maybe run (\p -> printGame >> conclude p)
+-- | Play a game against minimax!
+playTicTacToe = evalGame TicTacToe ["Puny Human" ::: human, "Minimax" ::: minimax]
+                         (run >> printScore)
+  where run = printGame >> step >>= maybe run (\p -> printGame >> return p)
 
---
--- Game instances
---
-
+-- Game instance
 instance Game TicTacToe where
-  type Move TicTacToe = Mark
+  type TreeType TicTacToe = Discrete
+  type Move  TicTacToe = Mark
   type State TicTacToe = Board
-  initState _ = start
-  runGame     = runTree
-  
-instance Searchable TicTacToe where
-  gameTree g = stateGameTree g who end markable (nextState g) pay
-  nextState _ b m = mark (who b) m b
+  gameTree _ = stateTreeD who end markable mark pay start
+
 
 --
--- Pretty printing
+-- * Pretty printing
 --
 
 instance Show Square where
@@ -83,10 +92,12 @@ instance Show Square where
   show O = "O"
   show E = " "
 
+-- | A string representation of the game board.
 showGame :: Board -> String
 showGame = concat . intersperse "\n" . intersperse line . map row . chunk 3 . map show
   where row [a,b,c] = printf " %s | %s | %s " a b c
         line = "-----------"
 
+-- | Print out the game during game execution.
 printGame :: GameM m TicTacToe => m ()
 printGame = gameState >>= liftIO . putStrLn . showGame
